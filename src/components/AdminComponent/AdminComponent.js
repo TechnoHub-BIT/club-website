974bbf5751598955a509917c5975457d657c334f
fazer1929 <abhishekagrawal8888@gmail.com>
2020-09-22 19:33:44 +0530

    Changes in Admin Header Button

--- src/components/AdminComponent/AdminComponent.js
+++ src/components/AdminComponent/AdminComponent.js
@@ -1,92 +1,90 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react';
-import {db} from '../../firebase';
-import { Row ,Container, Col } from 'react-bootstrap';
-import './adminComponent.css'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { db } from "../../firebase";
+import { Row, Container, Col } from "react-bootstrap";
+import "./adminComponent.css";
+import { useStateValue } from "../../StateProvider";
 
 function AdminComponent() {
-    
-    const [contacts, setContacts] = useState([]);
-    const [profiles, setProfiles] = useState([]);
+  const [contacts, setContacts] = useState([]);
+  const [profiles, setProfiles] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
 
-    useEffect(()=>{
-        db.collection('contacts').get().then(querySnapshot => {
-            const data = querySnapshot.docs.map(doc => doc.data());
-            console.log(data);
-            setContacts(data);
-        })
+  useEffect(() => {
+    db.collection("contacts")
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => doc.data());
+        console.log(data);
+        setContacts(data);
+      });
 
-        db.collection('members').get().then(querySnapshot => {
-            const data = querySnapshot.docs.map(doc => doc.data());
-            console.log(data);
-            setProfiles(data);
-        })
-    },[]);
+    db.collection("members")
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => doc.data());
+        console.log(data);
+        setProfiles(data);
+      });
+  }, []);
 
-    return (
-        <div>
-               
-                            {/* <Container> */}
-                                <Row>
-                                    <Col>
-                                    <h3 className="h3-text" >Profiles</h3>
-                                    <table id="example" className="display table">
-                                        <thead className="thead-dark" >
-                                            <tr>
-                                            <th scope="col" >Full Name</th>
-                                            <th scope="col" >Email</th>
-                                            <th scope="col" >Member</th>
-                                            <th scope="col" >Payment</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                         {profiles?.map((profile,i) => {
-                                        
-                                        return (
-                                            <tr key={i}>     
-                                            <td data-label="Full Name" >{profile.fullname}</td>
-                                            <td data-label="Email" >{profile.email}</td>
-                                            <td data-label="Branch" >{profile.member}</td>
-                                            <td data-label="Semester" >{profile.payment}</td>
-                                            </tr>
-                                            
-                                        );
-                                    
-                                        })}
-                                    </tbody>
-                                   
-                                    </table>
-                                    </Col>
-                                    <Col> 
-                                        <h3 className="h3-text" >Messages</h3>
-                                    <table id="example" className="display table">
-                                    <thead className="thead-dark">
-                                        <tr>
-                                            <th scope="col" >Name</th>
-                                            <th scope="col" >Email</th>
-                                            <th scope="col" >Message</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    {contacts?.map((contact,i) => {
-                                        
-                                        return (
-                                            <tr key={i}>     
-                                            <td data-label="Name" >{contact.name}</td>
-                                            <td data-label="Email" >{contact.email}</td>
-                                            <td data-label="Message" >{contact.message}</td>
-                                                </tr>
-                                            
-                                        );
-                                    
-                                        })}
-                                    </tbody>
-                                </table></Col>
-                                </Row>
-                            {/* </Container>  */}
-        </div>
-    )
+  return (
+    <div>
+      {/* <Container> */}
+      <Row>
+        <Col>
+          <h3 className="h3-text">Profiles</h3>
+          <table id="example" className="display table">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">Full Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Member</th>
+                <th scope="col">Payment</th>
+              </tr>
+            </thead>
+            <tbody>
+              {profiles?.map((profile, i) => {
+                return (
+                  <tr key={i}>
+                    <td data-label="Full Name">{profile.fullname}</td>
+                    <td data-label="Email">{profile.email}</td>
+                    <td data-label="Branch">{profile.member}</td>
+                    <td data-label="Semester">{profile.payment}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Col>
+        <Col>
+          <h3 className="h3-text">Messages</h3>
+          <table id="example" className="display table">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Message</th>
+              </tr>
+            </thead>
+            <tbody>
+              {contacts?.map((contact, i) => {
+                return (
+                  <tr key={i}>
+                    <td data-label="Name">{contact.name}</td>
+                    <td data-label="Email">{contact.email}</td>
+                    <td data-label="Message">{contact.message}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Col>
+      </Row>
+      {/* </Container>  */}
+    </div>
+  );
 }
 
-export default AdminComponent
+export default AdminComponent;
--- src/components/AdminComponent/AdminHeaderButton.js
+++ src/components/AdminComponent/AdminHeaderButton.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useStateValue } from "../../StateProvider";
+import { db, firebaseApp } from "../../firebase";
 
 function AdminHeader() {
-
   const [{ user }, dispatch] = useStateValue();
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  return (
-    <span>
-
-      {(user && true) ? (<p>Admin</p>): (<p></p>)}
-      
-    </span>
-  );
+  useEffect(() => {
+    if (user) {
+      db.collection("members")
+        .doc(user.uid)
+        .onSnapshot(function (doc) {
+          console.log("Current data: ", doc.data());
+          doc.data().id == 1 ? setIsAdmin(true) : setIsAdmin(false);
+        });
+    }
+  }, [user]);
+  return <span>{user && true ? <p>Admin</p> : <p></p>}</span>;
 }
 
 export default AdminHeader;
