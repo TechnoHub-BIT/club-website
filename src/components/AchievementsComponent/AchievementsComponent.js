68690ccf1cc3050250832c5678c03f3d039223fb
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2020-10-30 02:40:04 +0530

    Achievements Page Modifications

--- src/components/AchievementsComponent/AchievementsComponent.js
+++ src/components/AchievementsComponent/AchievementsComponent.js
@@ -1,16 +1,60 @@
 import React from "react";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from "react-router-dom";
+import SingleAchievement from "./SingleAchievementComponent/SingleAchievement";
+
+import "./Achievements.css";
 
 const achievements = (props) => {
     return(
         <React.Fragment>    
-            <div className="container">
-                <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Achievements</BreadcrumbItem>
-                </Breadcrumb>
+            <div className="achievementsCont">
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Achievements</BreadcrumbItem>
+                        </Breadcrumb>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <SingleAchievement path="./assets/images/achievements/eogsea.jpeg">
+                                It is a matter of pride and honor that one of the team
+                                members of TechnoHub competed in the Global Student
+                                Entrepreneurship Award (GSEA) with well-known
+                                entrepreneurs and won the cash prize worth Rs.1 Lakh. The
+                                winning members moved forward to the regionals which was
+                                held at Coimbatore.
+                            </SingleAchievement>
+                        </div>
+                        <div className="col-md-6">
+                            <SingleAchievement>
+                                Seriate to the event was EYANTRA which is an initiative to
+                                spread education in Embedded systems and Robotics by IIT
+                                Bombay sponsored by Ministry of Human Resource
+                                Development through the National Mission on Education
+                                through ICT (NMEICT). And it gives us immense pleasure
+                                and exaltation that more than 30+ teams registered for
+                                EYANTRA after promotions were done by TechnoHub in our
+                                institute. This year a team from TechnoHub managed
+                                successfully cracking ahead into the semis and showcases
+                                their project.
+                            </SingleAchievement>
+                        </div>
+                        <div className="col-md-6">
+                            <SingleAchievement>
+                                Aerial Painting Drone is one of the major projects in which
+                                few of the most talented and hardworking TechnoHub
+                                members are working. It is all about a simple drone which is
+                                capable of painting at any surface. It works on the simple
+                                principles of drone. And this drone was funded worth
+                                Rs.30,000 by the university. Seeing upon the progress, the
+                                university will fund more up to Rs.50,000.
+                            </SingleAchievement>
+                        </div>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
