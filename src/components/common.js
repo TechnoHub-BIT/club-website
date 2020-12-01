624ffb9589383075a900304b7644a621b30bb6af
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2020-12-01 20:23:19 +0530

    Navbar Resolved

--- src/components/common.js
+++ src/components/common.js
@@ -1,15 +1,15 @@
 const showNavbar = () => {
   const menuBtn = document.querySelector(".menuBtn i");
-  const list = document.querySelector(".navbarTop ul");
+  const navbarTop = document.querySelector(".navbarTop");
   const links = document.querySelectorAll(".navbarTop ul li a");
   menuBtn.addEventListener("click", () => {
-    list.classList.toggle("show");
+    navbarTop.classList.toggle("show");
     menuBtn.classList.toggle("show");
   });
 
   links.forEach(link => {
     link.addEventListener("click", () => {
-      list.classList.remove("show");
+      navbarTop.classList.remove("show");
       menuBtn.classList.remove("show");
     });
   });
