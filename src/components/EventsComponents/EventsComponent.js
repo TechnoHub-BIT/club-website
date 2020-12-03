4c049372d0cad875e5ea01907b55fd24622b4668
Aditya Deshmukh <adityadeshmukh168@gmail.com>
2020-12-03 22:29:37 +0530

    Added all events

--- src/components/EventsComponents/EventsComponent.js
+++ src/components/EventsComponents/EventsComponent.js
@@ -46,7 +46,103 @@ class EventsComponent extends Component {
                     <Col style={{alignSelf:"center"}} >
                       <CardBody className="events-card-body" >
                         <CardTitle className="events-card-title" >Hackathon</CardTitle>
-                        <CardSubtitle className="events-card-subtitle" >OCT 17 - 18</CardSubtitle>
+                        <CardSubtitle className="events-card-subtitle" >OCT 17 - 18 2020</CardSubtitle>
+                      </CardBody>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+              <Link to="/hultprize2020" >
+                <Card className="events-card">
+                  <Row>
+                    <Col style={{maxWidth: "350px"}} >
+                      <CardImg style={{alignSelf:"center"}} className="card_img" top width="100%" src="assets/images/events_img/HultPrize_nov2020.png" alt="Hult Prize BITD" />
+                      <div className="top-right">ONLINE</div>
+                    </Col>
+                    <Col style={{alignSelf:"center"}} >
+                      <CardBody className="events-card-body" >
+                        <CardTitle className="events-card-title" >Hult Prize 2021</CardTitle>
+                        <CardSubtitle className="events-card-subtitle" >Aug 23 - Nov 08</CardSubtitle>
+                      </CardBody>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+              <Link to="/hultprize2020" >
+                <Card className="events-card">
+                  <Row>
+                    <Col style={{maxWidth: "350px"}} >
+                      <CardImg style={{alignSelf:"center"}} className="card_img" top width="100%" src="assets/images/events_img/EV.png" alt="Hult Prize BITD" />
+                      <div className="top-right">ONLINE</div>
+                    </Col>
+                    <Col style={{alignSelf:"center"}} >
+                      <CardBody className="events-card-body" >
+                        <CardTitle className="events-card-title" >Electical Vehicle - By Suraj SD</CardTitle>
+                        <CardSubtitle className="events-card-subtitle" >OCT 04 2020</CardSubtitle>
+                      </CardBody>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+              <Link to="/eaglecad" >
+                <Card className="events-card">
+                  <Row>
+                    <Col style={{maxWidth: "350px"}} >
+                      <CardImg style={{alignSelf:"center"}} className="card_img" top width="100%" src="assets/images/events_img/EagleCAD.png" alt="Hult Prize BITD" />
+                      <div className="top-right">ONLINE</div>
+                    </Col>
+                    <Col style={{alignSelf:"center"}} >
+                      <CardBody className="events-card-body" >
+                        <CardTitle className="events-card-title" >EagleCAD Workshop</CardTitle>
+                        <CardSubtitle className="events-card-subtitle" >SEP 26 2020</CardSubtitle>
+                      </CardBody>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+              <Link to="/hultprize2020" >
+                <Card className="events-card">
+                  <Row>
+                    <Col style={{maxWidth: "350px"}} >
+                      <CardImg style={{alignSelf:"center"}} className="card_img" top width="100%" src="assets/images/events_img/HultPrize_jan2020.png" alt="Hult Prize BITD" />
+                      <div className="top-right2">OFFLINE</div>
+                    </Col>
+                    <Col style={{alignSelf:"center"}} >
+                      <CardBody className="events-card-body" >
+                        <CardTitle className="events-card-title" >Hult Prize 2020</CardTitle>
+                        <CardSubtitle className="events-card-subtitle" >JAN 16 - 23 2020</CardSubtitle>
+                      </CardBody>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+              <Link to="/hultprize2020" >
+                <Card className="events-card">
+                  <Row>
+                    <Col style={{maxWidth: "350px"}} >
+                      <CardImg style={{alignSelf:"center"}} className="card_img" top width="100%" src="assets/images/events_img/Robotics.png" alt="Hult Prize BITD" />
+                      <div className="top-right2">OFFLINE</div>
+                    </Col>
+                    <Col style={{alignSelf:"center"}} >
+                      <CardBody className="events-card-body" >
+                        <CardTitle className="events-card-title" >Robotics Workshop</CardTitle>
+                        <CardSubtitle className="events-card-subtitle" >SEP 28 - 29 2019</CardSubtitle>
+                      </CardBody>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+              <Link to="/hultprize2020" >
+                <Card className="events-card">
+                  <Row>
+                    <Col style={{maxWidth: "350px"}} >
+                      <CardImg style={{alignSelf:"center"}} className="card_img" top width="100%" src="assets/images/events_img/teche.jpeg" alt="Hult Prize BITD" />
+                      <div className="top-right2">OFFLINE</div>
+                    </Col>
+                    <Col style={{alignSelf:"center"}} >
+                      <CardBody className="events-card-body" >
+                        <CardTitle className="events-card-title" >Tech Expo @ OJAS 2019</CardTitle>
+                        <CardSubtitle className="events-card-subtitle" >APR 03 2019</CardSubtitle>
                       </CardBody>
                     </Col>
                   </Row>
