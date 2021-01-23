aa8505eb452c0de68b5d50a3546fb22fbb2933d8
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2021-01-24 00:07:42 +0530

    Basic Updates

--- src/components/CollaborationsComponent/CollaborationsComponent.js
+++ src/components/CollaborationsComponent/CollaborationsComponent.js
@@ -8,7 +8,9 @@ const collaborations = () => {
                 <div className="landing">
                     <div className="content">
                         <div className="heading">Ideas and<br/>Collaborations</div>
-                        <p className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <p className="text">
+                            Technohub aims at providing  an enriching experience for ideas to grow in a realistic setting and hence we work towards it by collaborating with the concerned industries or individuals who assume same goals as we do so that we grow together. Our current and active collaborators are 36 ink and headstart and we plan on expanding the variety of industries we collaborate with in order to accommodate most areas of interest and give exposure to more diverse ideas.
+                        </p>
                     </div>
                     <img src="./assets/images/collabs/background.jpg" alt="Collaborations" />
                 </div>
