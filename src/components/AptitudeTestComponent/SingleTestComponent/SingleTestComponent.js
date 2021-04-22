d1e64d705c7b7c01a174f6feda65cdde8c3188b6
narayan <narayan3119@gmail.com>
2021-04-22 20:55:12 +0530

    nothing

--- src/components/AptitudeTestComponent/SingleTestComponent/SingleTestComponent.js
+++ src/components/AptitudeTestComponent/SingleTestComponent/SingleTestComponent.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../../../firebase";
 import "./SingleTestComponent.css";
-import Moment from 'moment';
-import useCountDown from 'react-countdown-hook';
+import Moment from "moment";
+import useCountDown from "react-countdown-hook";
 
 const SingleTest = (props) => {
   const [tests, setTest] = useState([]);
@@ -21,14 +21,12 @@ const SingleTest = (props) => {
         endtime: Test.endtime,
         positivemarks: Test.positivemarks,
         negativemarks: Test.negativemarks,
-      questions:Test.questions
+        questions: Test.questions,
       });
     } else {
       console.log("No such test found!");
     }
   });
-
- 
 
   //Next and Previous Buttons
   const sectionChanger = (action, index) => {
@@ -36,32 +34,33 @@ const SingleTest = (props) => {
 
     const sections = document.querySelectorAll("section");
 
-    if(action === "next")
-      sections[index + 1].classList.add("active");
-    else
-      sections[index - 1].classList.add("active");
+    if (action === "next") sections[index + 1].classList.add("active");
+    else sections[index - 1].classList.add("active");
   };
 
   //CLear Selection Button
   const clearSelection = (name) => {
-    const radioBtns = document.querySelectorAll("input[type='radio'][name='" + name + "']")
+    const radioBtns = document.querySelectorAll(
+      "input[type='radio'][name='" + name + "']"
+    );
 
-    radioBtns.forEach(radioBtn => {
-      if(radioBtn.value === "Unanswered")
-        radioBtn.checked = true;
-      else
-        radioBtn.checked = false;
+    radioBtns.forEach((radioBtn) => {
+      if (radioBtn.value === "Unanswered") radioBtn.checked = true;
+      else radioBtn.checked = false;
     });
   };
 
   //Form Start Check
   const [form, setForm] = useState(false);
-  
+
   //Countdown Timer
-  const initialTime =  0.5 * 60 * 1000;
+  const initialTime = 0.5 * 60 * 1000;
   const interval = 1000;
 
-  const [timeLeft, { start, pause, resume, reset }] = useCountDown(initialTime, interval);
+  const [timeLeft, { start, pause, resume, reset }] = useCountDown(
+    initialTime,
+    interval
+  );
 
   let hours = parseInt(timeLeft / 3600000);
   let minutes = parseInt(timeLeft / 60000);
@@ -72,11 +71,11 @@ const SingleTest = (props) => {
     pause();
   }, []);
 
-  if(timeLeft === 0 && form) {
+  if (timeLeft === 0 && form) {
     setForm(false);
     console.warn("Time up!");
   }
-  
+
   const restart = React.useCallback(() => {
     const newTime = 0.2 * 60 * 1000;
     start(newTime);
@@ -87,7 +86,9 @@ const SingleTest = (props) => {
       <div className="singleTestCont">
         <h1 className="title">
           {tests.title}
-          <button type="button"><i className="fas fa-check"></i>&nbsp;&nbsp;Submit Test</button>
+          <button type="button">
+            <i className="fas fa-check"></i>&nbsp;&nbsp;Submit Test
+          </button>
         </h1>
         <div className="centreCard">
           <div className="left">
@@ -96,68 +97,149 @@ const SingleTest = (props) => {
                 <div className="testInstructions">
                   <h3 className="smallTitle">Test Instructions</h3>
                   <ul>
-                    <li><strong>Test Date:</strong> {Moment(tests.testdate).format('ll')}.</li>
-                    <li><strong>You can give the test between:</strong> {tests.starttime} to {tests.endtime}.</li>
-                    <li><strong>Test Duration:</strong> {tests.duration} minutes.</li>
-                    <li><strong>Total Marks:</strong> {tests.totalmarks}.</li>
-                    <li><strong>Marks for each Correct answer:</strong> {tests.positivemarks}.</li>
-                    <li><strong>Negative Marks for each Wrong answer:</strong> -{tests.negativemarks}.</li>
-                    
+                    <li>
+                      <strong>Test Date:</strong>{" "}
+                      {Moment(tests.testdate).format("ll")}.
+                    </li>
+                    <li>
+                      <strong>You can give the test between:</strong>{" "}
+                      {tests.starttime} to {tests.endtime}.
+                    </li>
+                    <li>
+                      <strong>Test Duration:</strong> {tests.duration} minutes.
+                    </li>
+                    <li>
+                      <strong>Total Marks:</strong> {tests.totalmarks}.
+                    </li>
+                    <li>
+                      <strong>Marks for each Correct answer:</strong>{" "}
+                      {tests.positivemarks}.
+                    </li>
+                    <li>
+                      <strong>Negative Marks for each Wrong answer:</strong> -
+                      {tests.negativemarks}.
+                    </li>
                   </ul>
                 </div>
                 <div className="navigation">
-                  <button type="button" className="startBtn" onClick={() => {setForm(true); sectionChanger("next", 0); restart();}}>Start Test&nbsp;&nbsp;<i className="fas fa-long-arrow-alt-right"></i></button>
+                  <button
+                    type="button"
+                    className="startBtn"
+                    onClick={() => {
+                      setForm(true);
+                      sectionChanger("next", 0);
+                      restart();
+                    }}
+                  >
+                    Start Test&nbsp;&nbsp;
+                    <i className="fas fa-long-arrow-alt-right"></i>
+                  </button>
                 </div>
               </section>
-              {
-                tests.questions &&
-                
+              {tests.questions &&
                 tests.questions.map((item, index) => {
                   return (
-                  
-                    <section ques-no={ index + 1 } key={index}>
-                      <h3 className="smallTitle">Question No. { index + 1 }</h3>
+                    <section ques-no={index + 1} key={index}>
+                      <h3 className="smallTitle">Question No. {index + 1}</h3>
                       <div className="question">{item.question}</div>
                       <div className="clearSelection">
-                        <button type="button" onClick={() => clearSelection(("option" + (index + 1)))}>Clear Selection</button>
+                        <button
+                          type="button"
+                          onClick={() => clearSelection("option" + (index + 1))}
+                        >
+                          Clear Selection
+                        </button>
                       </div>
                       <div className="options">
-                        <input type="radio" name={ "option" + (index + 1) } value="Unanswered" defaultChecked hidden />
+                        <input
+                          type="radio"
+                          name={"option" + (index + 1)}
+                          value="Unanswered"
+                          defaultChecked
+                          hidden
+                        />
                         <div>
-                          <input type="radio" name={ "option" + (index + 1) } id={ "optiona" + (index + 1) } value={item.op4} />&nbsp;&nbsp;
-                          (A) <label htmlFor={ "optiona" + (index + 1) }>{item.op1}</label>
+                          <input
+                            type="radio"
+                            name={"option" + (index + 1)}
+                            id={"optiona" + (index + 1)}
+                            value={item.op4}
+                          />
+                          &nbsp;&nbsp; (A){" "}
+                          <label htmlFor={"optiona" + (index + 1)}>
+                            {item.op1}
+                          </label>
                         </div>
                         <div>
-                          <input type="radio" name={ "option" + (index + 1) } id={ "optionb" + (index + 1) } value={item.op4} />&nbsp;&nbsp;
-                          (B) <label htmlFor={ "optionb" + (index + 1) }>{item.op2}</label>
+                          <input
+                            type="radio"
+                            name={"option" + (index + 1)}
+                            id={"optionb" + (index + 1)}
+                            value={item.op4}
+                          />
+                          &nbsp;&nbsp; (B){" "}
+                          <label htmlFor={"optionb" + (index + 1)}>
+                            {item.op2}
+                          </label>
                         </div>
                         <div>
-                          <input type="radio" name={ "option" + (index + 1) } id={ "optionc" + (index + 1) } value={item.op4} />&nbsp;&nbsp;
-                          (C) <label htmlFor={ "optionc" + (index + 1) }>{item.op3}</label>
+                          <input
+                            type="radio"
+                            name={"option" + (index + 1)}
+                            id={"optionc" + (index + 1)}
+                            value={item.op4}
+                          />
+                          &nbsp;&nbsp; (C){" "}
+                          <label htmlFor={"optionc" + (index + 1)}>
+                            {item.op3}
+                          </label>
                         </div>
                         <div>
-                          <input type="radio" name={ "option" + (index + 1) } id={ "optiond" + (index + 1) } value={item.op4} />&nbsp;&nbsp;
-                          (D) <label htmlFor={ "optiond" + (index + 1) }>{item.op4}</label>
+                          <input
+                            type="radio"
+                            name={"option" + (index + 1)}
+                            id={"optiond" + (index + 1)}
+                            value={item.op4}
+                          />
+                          &nbsp;&nbsp; (D){" "}
+                          <label htmlFor={"optiond" + (index + 1)}>
+                            {item.op4}
+                          </label>
                         </div>
                       </div>
                       <div className="navigation">
-                        {
-                          index === 0 ?
-                            <button type="button" className="prevBtn" disabled onClick={() => sectionChanger("next", (index + 1))}>
-                              <i className="fas fa-long-arrow-alt-left"></i>&nbsp;&nbsp;Previous
-                            </button>
-                                      :
-                            <button type="button" className="prevBtn" onClick={() => sectionChanger("prev", (index + 1))}>
-                              <i className="fas fa-long-arrow-alt-left"></i>&nbsp;&nbsp;Previous
-                            </button>
-                        }
-                        <button type="button" className="nextBtn" onClick={() => sectionChanger("next", (index + 1))}>
-                          Save & Next&nbsp;&nbsp;<i className="fas fa-long-arrow-alt-right"></i>
+                        {index === 0 ? (
+                          <button
+                            type="button"
+                            className="prevBtn"
+                            disabled
+                            onClick={() => sectionChanger("next", index + 1)}
+                          >
+                            <i className="fas fa-long-arrow-alt-left"></i>
+                            &nbsp;&nbsp;Previous
+                          </button>
+                        ) : (
+                          <button
+                            type="button"
+                            className="prevBtn"
+                            onClick={() => sectionChanger("prev", index + 1)}
+                          >
+                            <i className="fas fa-long-arrow-alt-left"></i>
+                            &nbsp;&nbsp;Previous
+                          </button>
+                        )}
+                        <button
+                          type="button"
+                          className="nextBtn"
+                          onClick={() => sectionChanger("next", index + 1)}
+                        >
+                          Save & Next&nbsp;&nbsp;
+                          <i className="fas fa-long-arrow-alt-right"></i>
                         </button>
                       </div>
                     </section>
                   );
-              })}
+                })}
             </form>
           </div>
           <div className="right">
@@ -165,14 +247,30 @@ const SingleTest = (props) => {
               <p>Time left: {hours + ":" + minutes + ":" + seconds}</p>
             </div>
             <div className="questionBtns">
-              <button type="button" className="question">1</button>
-              <button type="button" className="question">2</button>
-              <button type="button" className="question">3</button>
-              <button type="button" className="question">4</button>
-              <button type="button" className="question">5</button>
-              <button type="button" className="question">6</button>
-              <button type="button" className="question">7</button>
-              <button type="button" className="question">8</button>
+              <button type="button" className="question">
+                1
+              </button>
+              <button type="button" className="question">
+                2
+              </button>
+              <button type="button" className="question">
+                3
+              </button>
+              <button type="button" className="question">
+                4
+              </button>
+              <button type="button" className="question">
+                5
+              </button>
+              <button type="button" className="question">
+                6
+              </button>
+              <button type="button" className="question">
+                7
+              </button>
+              <button type="button" className="question">
+                8
+              </button>
             </div>
           </div>
         </div>
