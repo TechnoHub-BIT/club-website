b971bb4d156da6b1d04e9af3f8f9d4c8f3a80664
narayan <narayan3119@gmail.com>
2021-04-26 21:45:05 +0530

    ordered test list

--- src/components/AptitudeTestComponent/AnswerKeyComponent/AnswerKeyComponent.js
+++ src/components/AptitudeTestComponent/AnswerKeyComponent/AnswerKeyComponent.js
@@ -74,6 +74,8 @@ const AnswerKey = (props) => {
         }
       })}
       <h1>Your answers</h1>
+
+      
       {tests.options &&
         tests.options.map((items,index) => {
           return <div>{index+1}:  {items}</div>;
--- src/components/AptitudeTestComponent/TestsListComponent/TestsListComponent.js
+++ src/components/AptitudeTestComponent/TestsListComponent/TestsListComponent.js
@@ -12,7 +12,7 @@ const TestsList = () => {
 
   useEffect(() => {
     const fetchdata = async () => {
-      db.collection("Tests").orderBy('testdate').onSnapshot(function (data) {
+      db.collection("Tests").orderBy('testdate','desc').onSnapshot(function (data) {
         setTest(
           data.docs.map((doc) => ({
             ...doc.data(),
