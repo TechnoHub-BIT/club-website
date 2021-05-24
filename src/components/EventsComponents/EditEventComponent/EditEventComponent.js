247381b13f4a65002f2e91246ed971148697e774
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2021-05-24 18:49:34 +0530

    Changes

--- src/components/EventsComponents/EditEventComponent/EditEventComponent.js
+++ src/components/EventsComponents/EditEventComponent/EditEventComponent.js
@@ -17,7 +17,6 @@ import { render } from "@testing-library/react";
 import { resetWarningCache } from "prop-types";
 import { useParams } from "react-router";
 
-
 class EditEvent extends Component {
   state = {
     key: "",
@@ -63,9 +62,7 @@ class EditEvent extends Component {
       eventtype,
     } = this.state;
 
-    const updateRef = db
-      .collection("Events")
-      .doc(this.props.eventName);
+    const updateRef = db.collection("Events").doc(this.props.eventName);
     updateRef
       .set({
         eventtitle,
@@ -179,9 +176,7 @@ class EditEvent extends Component {
               />
             </div>
             <div className="input-group w50p">
-              <button type="submit">
-                Update Event
-              </button>
+              <button type="submit">Update Event</button>
             </div>
           </form>
         </div>
